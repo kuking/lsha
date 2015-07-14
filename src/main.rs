23ffe964864c55112da9e99d7a0613cd3c9e61d9
Eduardo ES Riccardi <ed@kukino.uk>
2015-07-14 23:15:35 +0100

    moving simpledir into a module as this starts to get a bit bigger

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,12 @@ extern crate crypto;
 extern crate docopt;
 extern crate libc;
 
+mod simpledir;
+use simpledir::SimpleDir;
+
 use docopt::Docopt;
 
 use std::{io, fs};
-use std::fs::Metadata;
-
-use libc::consts::os::posix88::*;
-
-// the following makes the whole thing unix only
-use std::os::unix::fs::MetadataExt;
 
 
 use std::string::String;
@@ -18,72 +15,6 @@ use std::error::Error;
 
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
-
-
-struct SimpleDir {
-    fname :String,
-    mdata :fs::Metadata,
-}
-
-impl SimpleDir {
-
-    fn mode_as_string(mode :&u16) -> String {
-        fn rwx(mode :&u16, rm :u16, wm :u16, xm :u16) -> String {
-            let mut st = String::new();
-            if mode & rm == rm { st.push('r') } else { st.push('-') };
-            if mode & wm == wm { st.push('w') } else { st.push('-') };
-            if mode & xm == xm { st.push('x') } else { st.push('-') };
-            return st;
-        }
-        fn dbpcs(mode :&u16) -> char {
-            if      mode & S_IFLNK == S_IFLNK { return 's' }
-            else if mode & S_IFREG == S_IFREG { return '-' }
-            else if mode & S_IFBLK == S_IFBLK { return 'b' }
-            else if mode & S_IFDIR == S_IFDIR { return 'd' }
-            else if mode & S_IFIFO == S_IFIFO { return 'p' }
-            else if mode & S_IFCHR == S_IFCHR { return 'c' }
-            else { return '?' }
-        }
-        return format!("{}{}{}{}", dbpcs(mode),
-                                 rwx(mode, S_IRUSR, S_IWUSR, S_IXUSR),
-                                 rwx(mode, S_IRGRP, S_IWGRP, S_IXGRP),
-                                 rwx(mode, S_IROTH, S_IWOTH, S_IXOTH)).to_string();
-    }
-
-    fn dump_as_string(self : &SimpleDir) -> String {
-        let mut st = String::new();
-        let mode = self.mdata.mode();
-        st = st + &SimpleDir::mode_as_string(&mode);
-        st = st + &format!("{:10} ", &self.mdata.len());
-        st = st + & self.fname;
-        return st;
-    }
-    fn new(de : std::fs::DirEntry) -> SimpleDir {
-        SimpleDir {
-            fname: de.file_name().to_os_string().into_string().unwrap(),
-            mdata: de.metadata().unwrap()
-        }
-    }
-}
-
-#[test]
-fn mode_as_string_tests() {
-    assert_eq!("?---------", SimpleDir::mode_as_string(&0));
-    assert_eq!("?rwx------", SimpleDir::mode_as_string(&(S_IRUSR+S_IWUSR+S_IXUSR)));
-    assert_eq!("?---rwx---", SimpleDir::mode_as_string(&(S_IRGRP+S_IWGRP+S_IXGRP)));
-    assert_eq!("?------rwx", SimpleDir::mode_as_string(&(S_IROTH+S_IWOTH+S_IXOTH)));
-    assert_eq!("?r--r--r--", SimpleDir::mode_as_string(&(S_IRUSR+S_IRGRP+S_IROTH)));
-    assert_eq!("?-w--w--w-", SimpleDir::mode_as_string(&(S_IWUSR+S_IWGRP+S_IWOTH)));
-    assert_eq!("?--x--x--x", SimpleDir::mode_as_string(&(S_IXUSR+S_IXGRP+S_IXOTH)));
-    assert_eq!("d---------", SimpleDir::mode_as_string(&S_IFDIR));
-    assert_eq!("b---------", SimpleDir::mode_as_string(&S_IFBLK));
-    assert_eq!("p---------", SimpleDir::mode_as_string(&S_IFIFO));
-    assert_eq!("c---------", SimpleDir::mode_as_string(&S_IFCHR));
-    assert_eq!("s---------", SimpleDir::mode_as_string(&S_IFLNK));
-    assert_eq!("drwxrwxrwx", SimpleDir::mode_as_string(
-      &(S_IFDIR+S_IRUSR+S_IWUSR+S_IXUSR+S_IRGRP+S_IWGRP+S_IXGRP+S_IROTH+S_IWOTH+S_IXOTH)));
-}
-
 
 
 struct LshaRunConfig {
@@ -111,7 +42,7 @@ fn do_it(sh : &mut Sha256, path :&String, cfg :&LshaRunConfig) -> Result<(), io:
     let mut data : Vec<_> = try!(fs::read_dir(path))
         .map( |rde| { SimpleDir::new(rde.unwrap()) } )
         .collect::<Vec<_>>();
-    data.sort_by(|a, b| a.fname.cmp(&b.fname));
+    data.sort_by(|a, b| a.fname().cmp(&b.fname()));
 
     for sd in data.iter() {
         let s = sd.dump_as_string();
@@ -125,11 +56,11 @@ fn do_it(sh : &mut Sha256, path :&String, cfg :&LshaRunConfig) -> Result<(), io:
 
     if cfg.be_recursive {
         for sd in data.iter() {
-            if sd.mdata.is_dir() {
+            if sd.mdata().is_dir() {
                 let mut temp = String::new();
                 temp.push_str(path);
                 temp.push('/');
-                temp.push_str(&sd.fname);
+                temp.push_str(&sd.fname());
                 do_it(sh, &temp, cfg).unwrap();
             }
         }
---
+++ src/simpledir.rs
@@ -0,0 +1,81 @@
+
+use libc::consts::os::posix88::*;
+
+use std::fs::DirEntry;
+use std::fs::Metadata;
+
+use std::os::unix::fs::MetadataExt;
+
+
+pub struct SimpleDir {
+    fname :String,
+    mdata :Metadata,
+}
+
+impl SimpleDir {
+
+    pub fn mdata(&self) -> &Metadata {
+        return &self.mdata;
+    }
+
+    pub fn fname(&self) -> &String {
+        return &self.fname;
+    }
+
+    fn mode_as_string(mode :&u16) -> String {
+        fn rwx(mode :&u16, rm :u16, wm :u16, xm :u16) -> String {
+            let mut st = String::new();
+            if mode & rm == rm { st.push('r') } else { st.push('-') };
+            if mode & wm == wm { st.push('w') } else { st.push('-') };
+            if mode & xm == xm { st.push('x') } else { st.push('-') };
+            return st;
+        }
+        fn dbpcs(mode :&u16) -> char {
+            if      mode & S_IFLNK == S_IFLNK { return 's' }
+            else if mode & S_IFREG == S_IFREG { return '-' }
+            else if mode & S_IFBLK == S_IFBLK { return 'b' }
+            else if mode & S_IFDIR == S_IFDIR { return 'd' }
+            else if mode & S_IFIFO == S_IFIFO { return 'p' }
+            else if mode & S_IFCHR == S_IFCHR { return 'c' }
+            else { return '?' }
+        }
+        return format!("{}{}{}{}", dbpcs(mode),
+                                 rwx(mode, S_IRUSR, S_IWUSR, S_IXUSR),
+                                 rwx(mode, S_IRGRP, S_IWGRP, S_IXGRP),
+                                 rwx(mode, S_IROTH, S_IWOTH, S_IXOTH)).to_string();
+    }
+
+    pub fn dump_as_string(self : &SimpleDir) -> String {
+        let mut st = String::new();
+        let mode = self.mdata.mode();
+        st = st + &SimpleDir::mode_as_string(&mode);
+        st = st + &format!("{:10} ", &self.mdata.len());
+        st = st + & self.fname;
+        return st;
+    }
+
+    pub fn new(de : DirEntry) -> SimpleDir {
+        SimpleDir {
+            fname: de.file_name().to_os_string().into_string().unwrap(),
+            mdata: de.metadata().unwrap()
+        }
+    }
+}
+
+#[test]
+fn mode_as_string_tests() {
+    assert_eq!("?---------", SimpleDir::mode_as_string(&0));
+    assert_eq!("?rwx------", SimpleDir::mode_as_string(&(S_IRUSR+S_IWUSR+S_IXUSR)));
+    assert_eq!("?---rwx---", SimpleDir::mode_as_string(&(S_IRGRP+S_IWGRP+S_IXGRP)));
+    assert_eq!("?------rwx", SimpleDir::mode_as_string(&(S_IROTH+S_IWOTH+S_IXOTH)));
+    assert_eq!("?r--r--r--", SimpleDir::mode_as_string(&(S_IRUSR+S_IRGRP+S_IROTH)));
+    assert_eq!("?-w--w--w-", SimpleDir::mode_as_string(&(S_IWUSR+S_IWGRP+S_IWOTH)));
+    assert_eq!("?--x--x--x", SimpleDir::mode_as_string(&(S_IXUSR+S_IXGRP+S_IXOTH)));
+    assert_eq!("d---------", SimpleDir::mode_as_string(&S_IFDIR));
+    assert_eq!("b---------", SimpleDir::mode_as_string(&S_IFBLK));
+    assert_eq!("p---------", SimpleDir::mode_as_string(&S_IFIFO));
+    assert_eq!("c---------", SimpleDir::mode_as_string(&S_IFCHR));
+    assert_eq!("s---------", SimpleDir::mode_as_string(&S_IFLNK));
+    assert_eq!("drwxrwxrwx", SimpleDir::mode_as_string(
+      &(S_IFDIR+S_IRUSR+S_IWUSR+S_IXUSR+S_IRGRP+S_IWGRP+S_IXGRP+S_IROTH+S_IWOTH+S_IXOTH)));
+}
