c951a3d19b1aab740dbc9ca6478dd61491b77ee7
Eduardo ES Riccardi <ed@kukino.uk>
2015-07-19 21:17:26 +0100

    refactoring to calculate the sha in a more standard way and multiple other simplifications

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,7 @@ mod simpledir;
 use simpledir::SimpleDir;
 
 use docopt::Docopt;
-
 use std::{io, fs};
-
-
-use std::string::String;
-use std::error::Error;
 
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
@@ -22,7 +17,8 @@ struct LshaRunConfig {
     do_file_checksum : bool,
     be_recursive    : bool,
     be_quiet        : bool,
-    incl_timestamps : bool
+    incl_timestamps : bool,
+    incl_hidden     : bool
 }
 
 impl LshaRunConfig {
@@ -32,12 +28,24 @@ impl LshaRunConfig {
             do_file_checksum: args.get_bool(&"-c"),
             be_recursive:  args.get_bool(&"-r"),
             be_quiet:  args.get_bool(&"-q"),
-            incl_timestamps: args.get_bool(&"-t")
+            incl_timestamps: args.get_bool(&"-t"),
+            incl_hidden: args.get_bool(&"-l")
         }
     }
 }
 
-fn do_it(sh : &mut Sha256, path :&String, cfg :&LshaRunConfig) -> Result<(), io::Error> {
+fn put(sh : &mut Sha256, cfg :&LshaRunConfig, st : &String) {
+    if !cfg.be_quiet {
+        print!("{}", st);
+    }
+    sh.input_str(&st);
+}
+
+fn do_path(sh : &mut Sha256, path :&String, cfg :&LshaRunConfig) -> Result<(), io::Error> {
+
+    if cfg.be_recursive {
+        put(sh, cfg, &format!("\n{}\n", path));
+    }
 
     let mut data : Vec<_> = try!(fs::read_dir(path))
         .map( |rde| { SimpleDir::new(rde.unwrap()) } )
@@ -45,23 +53,20 @@ fn do_it(sh : &mut Sha256, path :&String, cfg :&LshaRunConfig) -> Result<(), io:
     data.sort_by(|a, b| a.fname().cmp(&b.fname()));
 
     for sd in data.iter() {
-        let s = sd.dump_as_string();
-        if !cfg.be_quiet {
-          println!("{}", &s);
+        if cfg.incl_hidden || !sd.fname().starts_with('.') {
+            let s = sd.dump_as_string();
+            if cfg.do_file_checksum && sd.is_regular_file() {
+                // TODO: calculate file sha here
+                // and append to the text
+            }
+            put(sh, cfg, &format!("{}\n",&s));
         }
-        sh.input_str(&s);
-        sh.input_str(&"\n");
-
     }
 
     if cfg.be_recursive {
         for sd in data.iter() {
             if sd.mdata().is_dir() {
-                let mut temp = String::new();
-                temp.push_str(path);
-                temp.push('/');
-                temp.push_str(&sd.fname());
-                do_it(sh, &temp, cfg).unwrap();
+                do_path(sh, &(String::new() + &path + &"/" + &sd.fname()), cfg).unwrap();
             }
         }
     }
@@ -77,6 +82,7 @@ Usage: lsha [options] <PATH>
 Options: -c   Checksum file contents
          -r   Recursive
          -t   Use timestamps in checksum
+         -l   Include hidden files
          -q   quiet (don't output file details)
 ";
 
@@ -92,7 +98,7 @@ fn main() {
     let cfg = LshaRunConfig::from_docopt (args);
 
     let mut sh = Sha256::new();
-    match do_it(&mut sh, &cfg.path, &cfg) {
+    match do_path(&mut sh, &cfg.path, &cfg) {
         Ok(_)  => println!("lsha-256 {}", sh.result_str()),
         Err(e) => println!("error {}", e.to_string()),
     }
